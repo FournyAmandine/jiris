2c20edf4200f9c68cb29b6678ed4dbba456128c4
FournyAmandine <amandine.fourny@student.hepl.be>
2025-10-22 15:05:16 +0200

    Added components

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="{!! App::getLocale(); !!}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{!! $title !!}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
